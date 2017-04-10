02ffd8e9fd0ae6598c734f8f077945a16cd2fc04
Jeremy Wood <farachan@gmail.com>
2017-04-10 15:15:30 -0400

    Created some layout basics

--- src/main/java/conlife/ConlifeMain.java
+++ src/main/java/conlife/ConlifeMain.java
@@ -1,6 +1,10 @@
 package conlife;
 
+import net.miginfocom.swing.MigLayout;
+
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
+import java.awt.*;
 
 public class ConlifeMain extends JFrame {
 
@@ -11,5 +15,37 @@ public class ConlifeMain extends JFrame {
      */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new ConlifeMain().setVisible(true));
+    }
+
+    private ConlifeMain() {
+        setTitle("Conway's Game of Life (with Concurrency!)");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(this);
+        getContentPane().add(initComponents());
+        setPreferredSize(new Dimension(900, 700));
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    private JPanel initComponents() {
+        JPanel panel = new JPanel(new MigLayout("fill", "[grow]", "[shrink][grow][shrink]"));
+
+        JPanel settingPanel = new JPanel(new MigLayout("fill", "[grow]", "[]"));
+        settingPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
+        JLabel rulesLabel = new JLabel("Rules");
+        JTextField rulesField = new JTextField();
+        settingPanel.add(rulesLabel, "");
+        settingPanel.add(rulesField, "growx");
+
+
+        JPanel gamePanel = new JPanel();
+
+        JPanel buttonsPanel = new JPanel(new MigLayout());
+
+        panel.add(settingPanel, "growx, wrap");
+        panel.add(gamePanel, "wrap");
+        panel.add(buttonsPanel, "wrap");
+
+        return panel;
     }
 }
