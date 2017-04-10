19aa40ab38ec538e361eed7bf9d4aec4d4b101c7
Jeremy Wood <farachan@gmail.com>
2017-04-10 14:52:26 -0400

    Now is a runnable application

--- src/main/java/conlife/ConlifeMain.java
+++ src/main/java/conlife/ConlifeMain.java
@@ -1,4 +1,15 @@
 package conlife;
 
-public class ConlifeMain {
+import javax.swing.*;
+
+public class ConlifeMain extends JFrame {
+
+    /**
+     * Starts the application.
+     *
+     * @param args no required or optional args.
+     */
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new ConlifeMain().setVisible(true));
+    }
 }
