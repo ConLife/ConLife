6cacb67c65e883c2b3f6e3f4090d888b494f2456
2Lazy4This <ncoggins@unca.edu>
2017-04-25 14:11:38 -0400

    Changed args parsing and implemented pgm outputs.

--- src/main/java/conlife/console/ConlifeCLI.java
+++ src/main/java/conlife/console/ConlifeCLI.java
@@ -3,6 +3,8 @@ package conlife.console;
 import conlife.GameState;
 import conlife.Rules;
 import conlife.utils.Lif1_5Reader;
+import conlife.utils.PgmWriter;
+import static conlife.utils.PgmWriter.createPgmWriter;
 import java.awt.Dimension;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,47 +15,85 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class ConlifeCLI {
-    
+
     private static GameState gameState;
     private static Lif1_5Reader reader;
     private static int boardSize = 1000;
     private static int totalSteps = 500;
-    private static String inputFile = "./samples/LINEPUF.LIF";
+    private static String inFile = "./samples/LINEPUF.LIF";
     private static int threadCount = 4;
     private final static double NANOSECONDS_TO_MILLISECONDS = 1.0 / 1000000.0;
-    
-    public static void main(String[] args) throws FileNotFoundException, ParseException, Rules.RulesException, IOException {
-        if (args.length > 0) {
-            inputFile = args[0];
-            if (args.length > 1) {
-                boardSize = Integer.parseInt(args[1]);
-                if (args.length > 2) {
-                    totalSteps = Integer.parseInt(args[2]);
-                    if (args.length > 3) {
-                        threadCount = Integer.parseInt(args[3]);
-                    }
+    private static boolean outputs = false;
+    private static PgmWriter writer;
+
+    private static void parseArgs(String[] args) {
+        int argsi = 0;
+        while (args.length > argsi) {
+            if (args[argsi].charAt(0) == '-') {
+                switch (args[argsi].charAt(1)) {
+                    case 'f'://input file name
+                        inFile = args[argsi].substring(2);
+                        break;
+                    case 'b'://board size
+                        boardSize = Integer.parseInt(args[argsi].substring(2));
+                        break;
+                    case 's'://total steps
+                        totalSteps = Integer.parseInt(args[argsi].substring(2));
+                        break;
+                    case 't'://threads
+                        threadCount = Integer.parseInt(args[argsi].substring(2));
+                        break;
+                    case 'o'://ouputs wanted
+                        outputs = true;
+                        break;
                 }
+                argsi++;
             }
         }
-        reader = Lif1_5Reader.fromFile(new Dimension(boardSize, boardSize), new File(inputFile));
-        String[] init = reader.createBoardString('.', '*').split("\n");
-        gameState = GameState.createNewGame(init, '*', threadCount);
-        
-        long start = System.nanoTime();
+    }
+
+    private static void runGame() {
+        if (outputs) {
+            writer.createOutputForCurrentGameStep();
+        }
         for (int step = 0; step < totalSteps; step++) {
             gameState.processGameStep();
+            if (outputs) {
+                writer.createOutputForCurrentGameStep();
+            }
         }
-        
-        long end = System.nanoTime();
-        long time = end - start;
-        double totalTime = time * NANOSECONDS_TO_MILLISECONDS;
-        
-        String report = String.format("%s\t%d\t%d\t%d\t%f\n", inputFile, boardSize, totalSteps, threadCount, totalTime);
+    }
+
+    private static void report(double totalTime) {
+        String report = String.format("%s\t%d\t%d\t%d\t%f\n", inFile, boardSize, totalSteps, threadCount, totalTime);
         try {
             Files.write(Paths.get("timings.txt"), report.getBytes(), StandardOpenOption.APPEND);
         } catch (IOException e) {
             System.out.println(e.toString());
         }
+    }
+
+    private static void init() throws FileNotFoundException, ParseException, Rules.RulesException, IOException {
+        reader = Lif1_5Reader.fromFile(new Dimension(boardSize, boardSize), new File(inFile));
+        gameState = GameState.createNewGame(reader.createBoardString('.', '*').split("\n"), '*', threadCount);
+        if (outputs) {
+            writer = createPgmWriter(new File("./testOutput"), gameState);
+        }
+    }
+
+    private static double calcTime(long start){
+        long end = System.nanoTime();
+        long time = end - start;
+        return (time * NANOSECONDS_TO_MILLISECONDS);
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException, ParseException, Rules.RulesException, IOException {
+        parseArgs(args);
+        init();
+        long start = System.nanoTime();
+        runGame();
+        double totalTime = calcTime(start);
+        report(totalTime);
         System.exit(0);
     }
 }
