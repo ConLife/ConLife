872006923b79902353cf2a10707e7b36581e05ad
Nathan Coggins <ncoggins@unca.edu>
2017-04-25 09:23:55 -0400

    Added parsing for command line arguments.

--- src/main/java/conlife/console/ConlifeCLI.java
+++ src/main/java/conlife/console/ConlifeCLI.java
@@ -16,25 +16,39 @@ public class ConlifeCLI {
 
     private static GameState gameState;
     private static Lif1_5Reader reader;
-    private final static int BOARD_SIZE = 1000;
-    private final static int TOTAL_STEPS = 500;
-    private final static String INPUT_FILE = "./samples/LINEPUF.LIF";
+    private static int boardSize = 1000;
+    private static int totalSteps = 500;
+    private static String inputFile = "./samples/LINEPUF.LIF";
     private static int threadCount = 4;
     private final static double NANOSECONDS_TO_MILLISECONDS = 1.0 / 1000000.0;
 
     public static void main(String[] args) throws FileNotFoundException, ParseException, Rules.RulesException, IOException {
-        reader = Lif1_5Reader.fromFile(new Dimension(BOARD_SIZE, BOARD_SIZE), new File(INPUT_FILE));
+        if (args.length > 0) {
+            inputFile = args[0];
+            if (args.length > 1) {
+                boardSize = Integer.getInteger(args[1]);
+                if (args.length > 2) {
+                    totalSteps = Integer.getInteger(args[2]);
+                    if (args.length > 3) {
+                        threadCount = Integer.getInteger(args[3]);
+                    }
+                }
+            }
+        }
+        reader = Lif1_5Reader.fromFile(new Dimension(boardSize, boardSize), new File(inputFile));
         String[] init = reader.createBoardString('.', '*').split("\n");
         gameState = GameState.createNewGame(init, '*', threadCount);
 
         long start = System.nanoTime();
-        for (int step = 0; step < TOTAL_STEPS; step++) {
+        for (int step = 0; step < totalSteps; step++) {
             gameState.processGameStep();
         }
+        
         long end = System.nanoTime();
         long time = end - start;
         double totalTime = time * NANOSECONDS_TO_MILLISECONDS;
-        String report = String.format("%s\t%d\t%d\t%d\t%f\n", INPUT_FILE, BOARD_SIZE, TOTAL_STEPS, threadCount, totalTime);
+        
+        String report = String.format("%s\t%d\t%d\t%d\t%f\n", inputFile, boardSize, totalSteps, threadCount, totalTime);
         try {
             Files.write(Paths.get("timings.txt"), report.getBytes(), StandardOpenOption.APPEND);
         } catch (IOException e) {
